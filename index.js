0eebeef01b1b2227fd1b1a8e03e2d6d54bef3554
demkantor <demkantor@gmail.com>
2020-07-12 20:00:05 -0500

    sending test app to github

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    'mongodb://mongo:27017/docker-node-mongo',
+    { useUnifiedTopology: true, useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+const Item = require('./models/Item');
+
+app.get('/', (req, res) => {
+  Item.find()
+    .then(items => res.render('index', { items }))
+    .catch(err => res.status(404).json({ msg: 'No items found', err }));
+});
+
+app.post('/item/add', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  newItem.save()
+  .then(item => res.redirect('/'))
+  .catch(err => res.status(404).json({ msg: 'error adding new item', err }));
+});
+
+const port = 3000;
+
+app.listen(port, () => console.log('Server running...'));
